b99361a674f293dbd32bb0a1e1be09e8e7abfaaa
Muhammad Afify <muhammadafefy@gmail.com>
2024-08-13 19:45:56 +0000

    SQL

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- creates a trigger that resets
+
+DELIMITER $$
+CREATE TRIGGER reset 
+BEFORE UPDATE
+ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+
+DELIMITER;
